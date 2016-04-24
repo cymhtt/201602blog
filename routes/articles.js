be0a6cd8cb9fea2981e11b2c57e03717f6ad5bc6
zhangfengnodejs <83687401@qq.com>
2016-04-24 16:46:46 +0800

    发表文章的功能

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var models = require('../models');
+var auth = require('../middleware/auth');
 //路由的实例
 var router = express.Router();
 //注册
-router.get('/post', function(req, res, next) {
-  res.render('index', { title: '发表文章' });
+router.get('/add',auth.checkLogin, function(req, res, next) {
+  res.render('article/add', { title: '发表文章' });
 });
 
+router.post('/add',auth.checkLogin,function(req,res,next){
+  var article = req.body;
+  //把当前登陆的用户的ID赋给user
+  article.user = req.session.user._id;
+  models.Article.create(article,function(err,doc){
+    if(err){
+      req.flash('error','文章发表失败!');
+    }else{
+      req.flash('success','文章发表成功');
+      res.redirect('/');
+    }
+  });
+});
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var markdown = require('markdown').markdown;
+var models = require('../models');
 //调用Router方法生成一个路由的实例
 var router = express.Router();
 
@@ -7,7 +9,15 @@ var router = express.Router();
  * listener 指定回调监听函数
  */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '首页'});
+  //user 字符串 对象 user.avatar
+  //先查找 然后把user字符串转成user对象
+  models.Article.find({}).populate('user').exec(function(err,articles){
+    articles.forEach(function(article){
+      article.content = markdown.toHTML(article.content);
+    });
+    res.render('index', { articles: articles});
+  });
+
 });
 
 module.exports = router;
